6cc5ce3ff79af9821a6061c51a0c6c8097f71d2f
Yosr Jabeur <yosr.jabeur10@gmail.com>
2024-11-12 10:36:57 +0100

    first commit

--- app/src/main/java/com/example/monapp/MainActivity.kt
+++ app/src/main/java/com/example/monapp/MainActivity.kt
@@ -1,26 +1,20 @@
-package com.example.monapp
-
-import ecrans.Screen
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
 import com.example.monapp.ui.theme.MonAppTheme
+import ecrans.AppNavHost
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MonAppTheme {
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Screen()
-                }
+                val navController = rememberNavController()
+                AppNavHost(navController = navController)
             }
         }
     }
